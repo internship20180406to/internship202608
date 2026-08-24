cd62323b516539fa1c164326f8ab5a354cd40ba4
ffg-training <internship0216to0222@gmail.com>
2026-08-24 17:40:59 +0900

    亀澤　エラー解決

--- bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
+++ bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
@@ -28,13 +28,8 @@ public class BankLoanController {
     public String confirmation(@ModelAttribute BankLoanForm bankLoanForm, Model model) {
         bankLoanForm.setBankName("ながれぼし銀行");
         model.addAttribute("bankName", bankLoanForm.getBankName());
-        model.addAttribute("branchName", bankLoanForm.getBranchName());
-        model.addAttribute("bankAccountType", bankLoanForm.getBankAccountType());
         model.addAttribute("bankAccountNum", bankLoanForm.getBankAccountNum());
 
-        model.addAttribute("name", bankLoanForm.getName());
-        model.addAttribute("money", bankLoanForm.getMoney());
-        model.addAttribute("transferDatetime", bankLoanForm.getTransferDateTime());
 
         model.addAttribute("bankLoanApplication", bankLoanForm);
         return "bankLoanConfirmation";
