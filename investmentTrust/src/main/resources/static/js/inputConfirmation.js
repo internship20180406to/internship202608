411798653420b83ff84825656d49d8399d3d39ce
WID664\ffg-training <k.kikukawa@zdf.jp>
2026-08-25 16:34:40 +0900

    口座番号入力エラー処理 実装

--- investmentTrust/src/main/resources/static/js/inputConfirmation.js
+++ investmentTrust/src/main/resources/static/js/inputConfirmation.js
@@ -3,7 +3,8 @@ const inputName = document.getElementById("name");
 const inputAccountNum = document.getElementById("number");
 const inputMoney = document.getElementById("money");
 const name_error = document.getElementById("name_error");
-const accountnum_error = document.getElementById("accountnum_error");
+const accountnum_error_null = document.getElementById("accountnum_error_null");
+const accountnum_error_digit = document.getElementById("accountnum_error_digit");
 const money_error = document.getElementById("money_error");
 
     confirmButton.addEventListener('click', (e) => {
@@ -14,10 +15,17 @@ const money_error = document.getElementById("money_error");
             name_error.setAttribute('hidden', '');      //  setAribute:要素の編集
         }
         if (inputAccountNum.value === "") {
-            accountnum_error.removeAttribute('hidden');
+            accountnum_error_null.removeAttribute('hidden');
+            accountnum_error_digit.setAttribute('hidden', '');
             e.preventDefault();
-        } else {
-            accountnum_error.setAttribute('hidden', '');
+        } else if (String(inputAccountNum.value).length !== 7 || inputAccountNum.value < 0) {
+            accountnum_error_null.setAttribute('hidden', '');
+            accountnum_error_digit.removeAttribute('hidden');
+            e.preventDefault();
+        }
+        else {
+            accountnum_error_null.setAttribute('hidden', '');
+            accountnum_error_digit.setAttribute('hidden', '');
         }
         if (inputMoney.value === "") {
             money_error.removeAttribute('hidden');
