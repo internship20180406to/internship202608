144ce9c251052383d8ca9b45a9dc717e5b3c0a1b
WID664\ffg-training <k.kikukawa@zdf.jp>
2026-08-26 10:00:09 +0900

    ダイアログ キャンセルボタン 有効化

--- investmentTrust/src/main/resources/static/js/investmentTrustConfirmation.js
+++ investmentTrust/src/main/resources/static/js/investmentTrustConfirmation.js
@@ -1,4 +1,7 @@
-const submitButton = document.getElementById("submit")
-    submitButton.addEventListener('click', (e) => {
-      console.log(confirm("操作を実行します"))
-    })
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", (e) => {
+  if (!confirm("操作を実行します")) {
+    e.preventDefault();
+  }
+});
