961d892f6192d4d89413fda16570c2dfee429a54
ffg-training <internship0216to0222@gmail.com>
2026-08-27 13:15:19 +0900

    確認画面にもタイマーを追加、スクロールしてもタイマーが右上に表示されるように変更

--- bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
+++ bankLoan/src/main/java/com/example/internship/controller/BankLoanController.java
@@ -5,9 +5,7 @@ import com.example.internship.service.ApplyBankLoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -15,9 +13,8 @@ import java.util.Map;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Map;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BankLoanController {
